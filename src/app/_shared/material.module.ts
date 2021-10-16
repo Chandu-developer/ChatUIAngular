e445068dfcc2d405c0e78acf32942ac7c79f9e09
Ravi Chandu Kalla <ravichandu.kalla@uniquode.in>
2021-10-16 18:23:22 +0530

    added new chat container and no convo msg

--- src/app/_shared/material.module.ts
+++ src/app/_shared/material.module.ts
@@ -4,14 +4,17 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDividerModule} from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list'; 
 
 
 const Material_MODULES = [
   MatSidenavModule,
   MatCardModule,
   MatIconModule,
-  MatFormFieldModule
-  
+  MatFormFieldModule,
+  MatDividerModule,
+  MatListModule,
 ]
 
 @NgModule({
